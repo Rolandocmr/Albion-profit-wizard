00f120e3a173aa35ead538d1ab247630dbd4f2b9
Rolandocmr <rolandocmr@gmail.com>
2020-09-18 18:33:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,30 +14,61 @@ def createItemList(desiredItemType):
   return string_search
 
 
-def createAlbionUrl(itemlist, citylistId, itemQuality):
-    albion_api_link = f"https://www.albion-online-data.com/api/v2/stats/prices/{itemlist}?locations={citylistId}&qualities={itemQuality}"
+#def createAlbionUrl(itemlist, citylistId, itemQuality):
+def createAlbionUrl(itemlist):
+    #albion_api_link = f"https://www.albion-online-data.com/api/v2/stats/prices/{itemlist}?locations={citylistId}&qualities={itemQuality}"
+    albion_api_link = f"https://www.albion-online-data.com/api/v2/stats/prices/{itemlist}?locations={all_cities}"
     return albion_api_link
 
 
 # city list
 all_cities = "Caerleon,Bridgewatch,Lymhurst,Fortsterling,Thetford,Martlock"
-city_list = [all_cities, "Black Market" "Caerleon", "Bridgewatch", "Lymhurst", "Fortsterling", "Thetford", "Martlock"]
+city_list = ["Black Market", "Caerleon", "Bridgewatch", "Lymhurst", "Fortsterling", "Thetford", "Martlock", all_cities]
 
 # item list
-armor_query = ["T4_ARMOR", "T5_ARMOR", "T6_ARMOR", "T7_ARMOR", "T8_ARMOR", "T4_HEAD", "T5_HEAD", "T6_HEAD", "T7_HEAD",
-               "T8_HEAD", "T4_SHOES", "T5_SHOES", "T6_SHOES", "T7_SHOES", "T8_SHOES"]
-weapons_query = ["T4_MAIN", "T5_MAIN", "T6_MAIN", "T7_MAIN", "T8_MAIN", "T4_2H", "T5_2H", "T6_2H", "T7_2H", "T8_2H"]
-raw_materials_query = ["_ORE", "_HIDE", "_FIBER", "_WOOD", "_ROCK"]
-ref_materials_query = ["_METALBAR", "_LEATHER", "_FABRIC", "_WOOD", "_ROCK"]
+armor_query = ["ARMOR", "HEAD", "SHOES"]
+tier_query = ["T4", "T5", "T6", "T7", "T8"]
+refine_shit = ["", "_LEVEL1", "_LEVEL2", "_LEVEL3"]
+rarity_query = ["", "@1", "@2", "@3"]
+weapons_query = ["MAIN", "2H"]
+raw_materials_query = ["ORE", "HIDE", "FIBER", "WOOD", "ROCK"]
+ref_materials_query = ["METALBAR", "LEATHER", "CLOTH", "PLANK", "STONE"]
 all_items = armor_query + weapons_query + raw_materials_query + ref_materials_query
 search_promt = [all_items, armor_query, weapons_query, raw_materials_query, ref_materials_query]
 
+# item category
+# item choice (not for now)
+# tier
+# enchant
+
+# tier_itemChoice_refineShit_rarity
+"T4_CLOTH",
+"T5_CLOTH",
+"T6_CLOTH",
+"T7_CLOTH",
+"T8_CLOTH",
+"T4_CLOTH_LEVEL1@1",
+"T5_CLOTH_LEVEL1@1",
+"T6_CLOTH_LEVEL1@1",
+"T7_CLOTH_LEVEL1@1",
+"T8_CLOTH_LEVEL1@1",
+"T4_CLOTH_LEVEL2@2",
+"T5_CLOTH_LEVEL2@2",
+"T6_CLOTH_LEVEL2@2",
+"T7_CLOTH_LEVEL2@2",
+"T8_CLOTH_LEVEL2@2",
+"T4_CLOTH_LEVEL3@3",
+"T5_CLOTH_LEVEL3@3",
+"T6_CLOTH_LEVEL3@3",
+"T7_CLOTH_LEVEL3@3",
+"T8_CLOTH_LEVEL3@3",
+
+
 # input general
 ##city choice
-print(
-    "from which City?\n0.all\n1.blackmarket\n2.caerleon\n3.bridgewatch\n4.Lymhurst\n5.Fortsterling\n6.Thetford\n7.Martlock\n\n")
-string_city_result = input("Type the city number: ")
-int_city_result = int(string_city_result)
+#print("from which City?\n0.blackmarket\n1.caerleon\n2.bridgewatch\n3.Lymhurst\n4.Fortsterling\n5.Thetford\n6.Martlock\n\n7.all\n")
+#string_city_result = input("Type the city number: ")
+#int_city_result = int(string_city_result)
 ##item choice
 print("from which Type?\n0.All\n1.Armor\n2.weapons\n3.Raw_materials\n4.Ref_materials\n\n")
 string_item_result = input("Type the quality number: ")
@@ -45,14 +76,20 @@ int_item_result = int(string_item_result)
 desiredItemList = createItemList(search_promt[int_item_result])
 
 ##quality choice
-print("from which Quality?\n0.all\n1.one\n2.two\n3.three\n4.four\n5.five\n\n")
-string_quality = input("Type the quality number: ")
-int_quality = int(string_quality)
+#print("from which Quality?\n0.one\n1.two\n2.three\n3.four\n4.five\n\n")
+#string_quality = input("Type the quality number: ")
+#int_quality = int(string_quality)
 
-albion_api_link = createAlbionUrl(desiredItemList, city_list[int_city_result], int_quality)
+#albion_api_link = createAlbionUrl(desiredItemList, city_list[int_city_result], int_quality)
+albion_api_link = createAlbionUrl(desiredItemList)
 
 # asking the API for shit
 print(albion_api_link)
-# albiondata = requests.get(albion_api_link)
+
+albiondata = requests.get(albion_api_link)
 # print(albiondata.status_code)
 # print(albiondata.json())
+
+text_file = open("albion_data.txt", "w")
+n = text_file.write(albiondata)
+text_file.close()
